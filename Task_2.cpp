2aedcaf45193834b2b8df171f06aa449bfa4e6cf
SHAFQAT ULLAH KHAN <f2025-0905@bnu.edu.pk>
2025-10-11 01:36:04 +0500

    update Task 2

---
+++ Task_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    string english;
+    string maths;
+    string physics;
+    string iCT;
+    string pF;
+
+    cout << "Enter your English Marks: " << endl;
+    cin >> english;
+
+    cout << "Enter Your Physics marks: " << endl;
+    cin >> physics;
+
+    cout << "Enter Your ICT marks: " << endl;
+    cin >> iCT;
+
+    cout << "Enter Your PF marks: " << endl;
+    cin >> pF;
+
+
+
+}
